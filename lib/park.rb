c444eded5987a52e96724072ea044e7059585931
dariush abbasi <dariushem@yahoo.com>
2016-01-05 22:57:38 +0330

    add some scaffold command

--- lib/park.rb
+++ lib/park.rb
@@ -11,18 +11,24 @@ module Park
   def init_config
     if !File.exist?(Dir.home + "/.park.yml")
       system 'touch ~/.park.yml'
+      put users.to_yml
     else
       puts "config file already exists."
     end
   end
 
   # git configs list
-  def list
+  def list_accounts
     config_file = File.open Dir.home + "/.park.yml"
     config = YAML.load(config_file)
     puts config
     config_file.close
   end
 
+  def add_account
+  end
+
+  def switch_account(username=nil)
+  end
 
 end
