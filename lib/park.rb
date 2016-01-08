9c82439d6e114b13756d7d0be0e40d1c253ddd43
dariush abbasi <dariushem@yahoo.com>
2016-01-08 21:26:12 +0330

    complete add command

--- lib/park.rb
+++ lib/park.rb
@@ -16,7 +16,7 @@ module Park
       g = Git.open(Dir.pwd)
 
       users = {}
-      users[:default] = {
+      users['default'] = {
         :name => g.config('user.name'),
         :email => g.config('user.email')
       }
@@ -29,12 +29,20 @@ module Park
     end
   end
 
+  def init_config?
+    if File.exist?(Dir.home + "/.park.yml")
+      return true
+    else
+      return false
+    end
+  end
+
   # git configs list
   def list_accounts
     config_file = File.open Dir.home + "/.park.yml"
     config = YAML.load(config_file)
     if config.is_a?(Hash) then
-      config.each_pair do |key, value|
+      config.each_key do |key|
         puts key
       end
     end
@@ -42,6 +50,33 @@ module Park
   end
 
   def add_account
+    if init_config?
+
+      puts "Your name : "
+      name = STDIN.gets
+
+      puts "Your email : "
+      email = STDIN.gets
+
+      puts "Account name : "
+      username = STDIN.gets.chomp
+
+      users_file = File.open Dir.home + "/.park.yml"
+      users = YAML.load(users_file)
+
+      users[username] = {
+        :name => name,
+        :email => email
+      }
+
+      File.open(Dir.home + '/.park.yml', 'w') do |f|
+        f.write(users.to_yaml)
+      end
+
+    else
+      puts "first init pars : `pars init`"
+    end
+
   end
 
   def switch_account(username=nil)
